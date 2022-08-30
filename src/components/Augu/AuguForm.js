0b7a76bccce6f2082794a8ff12986252027e1de1
@aTolaba <tolaba.augusto@gmail.com>
2022-08-30 16:39:50 -0300

    cambios de colores y agregado un algo en la seccion augu :3

--- src/components/Augu/AuguForm.js
+++ src/components/Augu/AuguForm.js
@@ -1,12 +1,17 @@
 import classes from "./AuguForm.module.css";
 import Card from "../UI/Card";
+import img from "../../assets/images.png";
 
 const AuguForm = () => {
   return (
     <section className={classes.starting}>
-      <Card>
-        <h1>Augusto</h1>
-      </Card>
+      <h1>Sección Augu</h1>
+      <section className={classes.augu}>
+        <h2>Jarvis - NuMA</h2>
+        <p>
+          <img src={img} alt="Un Augusto durmiendo" />
+        </p>
+      </section>
     </section>
   );
 };
