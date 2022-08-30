06e0e87ae933ceea6054171f1bbc6b41d9c5f08d
@aTolaba <tolaba.augusto@gmail.com>
2022-08-30 11:17:45 -0300

    cambios visuales y botones :3

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,16 @@ import { Fragment } from "react";
 import classes from "./Layout.module.css";
 import MainNavigation from "./MainNavigation";
 
+import mealsImage from "../../assets/meals.jpg";
+
 const Layout = (props) => {
   return (
     <Fragment>
       <MainNavigation />
-      <main>{props.children}</main>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
+      </div>
+      <main className={classes.MainNavigation}>{props.children}</main>
     </Fragment>
   );
 };
--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 
 import AuthContext from "../../store/auth-context";
@@ -8,12 +8,81 @@ const MainNavigation = () => {
   const history = useHistory();
   const authCtx = useContext(AuthContext);
 
+  const [btnIsHighlightedAugu, setBtnIsHighlightedAugu] = useState(false);
+  const [btnIsHighlightedEma, setBtnIsHighlightedEma] = useState(false);
+  const [btnIsHighlightedMarga, setBtnIsHighlightedMarga] = useState(false);
+  const [btnIsHighlightedNancy, setBtnIsHighlightedNancy] = useState(false);
+
   const isLoggedIn = authCtx.isLoggedIn;
   const email = localStorage.getItem("email");
 
   const logoutHandler = () => {
     authCtx.logout();
     history.replace("/auth");
+  };
+
+  const btnClassesAugu = `${classes.buttonAugu} ${
+    btnIsHighlightedAugu ? classes.bump : ""
+  }`;
+
+  const btnClassesEma = `${classes.buttonEma} ${
+    btnIsHighlightedEma ? classes.bump : ""
+  }`;
+
+  const btnClassesMarga = `${classes.buttonMarga} ${
+    btnIsHighlightedMarga ? classes.bump : ""
+  }`;
+
+  const btnClassesNancy = `${classes.buttonNancy} ${
+    btnIsHighlightedNancy ? classes.bump : ""
+  }`;
+
+  const clickHandlerAugu = (event) => {
+    setBtnIsHighlightedAugu(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlightedAugu(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  };
+
+  const clickHandlerEma = (event) => {
+    setBtnIsHighlightedEma(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlightedEma(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  };
+
+  const clickHandlerMarga = (event) => {
+    setBtnIsHighlightedMarga(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlightedMarga(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  };
+
+  const clickHandlerNancy = (event) => {
+    setBtnIsHighlightedNancy(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlightedNancy(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
   };
 
   return (
@@ -26,25 +95,49 @@ const MainNavigation = () => {
           {isLoggedIn &&
             (email === "augu@test.com" || email === "test@test.com") && (
               <li>
-                <Link to="/Augu">Augusto</Link>
+                <Link
+                  className={btnClassesAugu}
+                  onClick={clickHandlerAugu}
+                  to="/Augu"
+                >
+                  Augusto
+                </Link>
               </li>
             )}
           {isLoggedIn &&
             (email === "ema@test.com" || email === "test@test.com") && (
               <li>
-                <Link to="/Ema">Emanuel</Link>
+                <Link
+                  className={btnClassesEma}
+                  onClick={clickHandlerEma}
+                  to="/Ema"
+                >
+                  Emanuel
+                </Link>
               </li>
             )}
           {isLoggedIn &&
             (email === "marga@test.com" || email === "test@test.com") && (
               <li>
-                <Link to="/Marga">Margarita</Link>
+                <Link
+                  className={btnClassesMarga}
+                  onClick={clickHandlerMarga}
+                  to="/Marga"
+                >
+                  Margarita
+                </Link>
               </li>
             )}
           {isLoggedIn &&
             (email === "nancy@test.com" || email === "test@test.com") && (
               <li>
-                <Link to="/Nancy">Nancy</Link>
+                <Link
+                  className={btnClassesNancy}
+                  onClick={clickHandlerNancy}
+                  to="/Nancy"
+                >
+                  Nancy
+                </Link>
               </li>
             )}
         </ul>
