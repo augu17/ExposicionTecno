a010d35d330d12f2cc9df39c8068eea8bedfc3e6
@aTolaba <tolaba.augusto@gmail.com>
2022-08-30 11:48:03 -0300

    mas cambios en las vistas :#

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,15 @@
 import { Fragment } from "react";
-import classes from "./Layout.module.css";
+// import classes from "./Layout.module.css";
 import MainNavigation from "./MainNavigation";
-
-import mealsImage from "../../assets/meals.jpg";
 
 const Layout = (props) => {
   return (
     <Fragment>
       <MainNavigation />
-      <div className={classes["main-image"]}>
+      {/* <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food!" />
-      </div>
-      <main className={classes.MainNavigation}>{props.children}</main>
+      </div> */}
+      <main>{props.children}</main>
     </Fragment>
   );
 };
