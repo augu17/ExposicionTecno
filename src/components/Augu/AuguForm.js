fc0cdc7ddaac1cad14a6a576f3ef2ab49b5c4f59
@aTolaba <tolaba.augusto@gmail.com>
2022-08-31 11:56:29 -0300

    agregadas imagenes y el slider en augu :3

--- src/components/Augu/AuguForm.js
+++ src/components/Augu/AuguForm.js
@@ -1,16 +1,33 @@
+import React, { useState, useEffect } from "react";
+import Carousel from "better-react-carousel";
+
 import classes from "./AuguForm.module.css";
-import Card from "../UI/Card";
-import img from "../../assets/images.png";
+// import Card from "../UI/Card";
+// import img from "../../assets/images.png";
+import imgJarvis0 from "../../assets/Jarvis-Role-0.png";
+import imgJarvis1 from "../../assets/Jarvis-Role-1.png";
+import imgJarvis2 from "../../assets/Jarvis-Role-2.png";
+import imgJarvis3 from "../../assets/Jarvis-Role-3.png";
 
 const AuguForm = () => {
   return (
     <section className={classes.starting}>
       <h1>Sección Augu</h1>
       <section className={classes.augu}>
-        <h2>Jarvis - NuMA</h2>
-        <p>
-          <img src={img} alt="Un Augusto durmiendo" />
-        </p>
+        <Carousel cols={1} rows={1} gap={5} loop scrollSnap>
+          <Carousel.Item>
+            <img className={classes.img2} src={imgJarvis0} />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className={classes.img} src={imgJarvis1} />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className={classes.img} src={imgJarvis2} />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className={classes.img2} src={imgJarvis3} />
+          </Carousel.Item>
+        </Carousel>
       </section>
     </section>
   );
