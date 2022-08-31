3f5c45dc596e39ee7230e4fa5b20d2bc4ea843c1
@aTolaba <tolaba.augusto@gmail.com>
2022-08-31 13:05:27 -0300

    a comer :3

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -3,6 +3,7 @@ import { Link, useHistory } from "react-router-dom";
 
 import AuthContext from "../../store/auth-context";
 import classes from "./MainNavigation.module.css";
+import logoTecno from "../../assets/logo-tecno.png";
 
 const MainNavigation = () => {
   const history = useHistory();
@@ -89,55 +90,100 @@ const MainNavigation = () => {
     <header className={classes.header}>
       <nav>
         <ul>
+          {/* {!isLoggedIn && (
+            <li>
+              <img className={classes.img} src={logoTecno}></img>
+            </li>
+          )} */}
           <li>
             <Link to="/">Inicio</Link>
           </li>
           {isLoggedIn &&
             (email === "augu@test.com" || email === "test@test.com") && (
               <li>
-                <Link
-                  className={btnClassesAugu}
-                  onClick={clickHandlerAugu}
-                  to="/Augu"
-                >
-                  Augusto
-                </Link>
+                {window.innerWidth > 768 ? (
+                  <Link
+                    className={btnClassesAugu}
+                    onClick={clickHandlerAugu}
+                    to="/Augu"
+                  >
+                    Augusto
+                  </Link>
+                ) : (
+                  <Link
+                    className={classes.btnChico}
+                    onClick={clickHandlerAugu}
+                    to="/Augu"
+                  >
+                    A
+                  </Link>
+                )}
               </li>
             )}
           {isLoggedIn &&
             (email === "ema@test.com" || email === "test@test.com") && (
               <li>
-                <Link
-                  className={btnClassesEma}
-                  onClick={clickHandlerEma}
-                  to="/Ema"
-                >
-                  Emanuel
-                </Link>
+                {window.innerWidth > 768 ? (
+                  <Link
+                    className={btnClassesEma}
+                    onClick={clickHandlerEma}
+                    to="/Ema"
+                  >
+                    Emanuel
+                  </Link>
+                ) : (
+                  <Link
+                    className={classes.btnChico}
+                    onClick={clickHandlerEma}
+                    to="/Ema"
+                  >
+                    E
+                  </Link>
+                )}
               </li>
             )}
           {isLoggedIn &&
             (email === "marga@test.com" || email === "test@test.com") && (
               <li>
-                <Link
-                  className={btnClassesMarga}
-                  onClick={clickHandlerMarga}
-                  to="/Marga"
-                >
-                  Margarita
-                </Link>
+                {window.innerWidth > 768 ? (
+                  <Link
+                    className={btnClassesMarga}
+                    onClick={clickHandlerMarga}
+                    to="/Marga"
+                  >
+                    Margarita
+                  </Link>
+                ) : (
+                  <Link
+                    className={classes.btnChico}
+                    onClick={clickHandlerMarga}
+                    to="/Marga"
+                  >
+                    M
+                  </Link>
+                )}
               </li>
             )}
           {isLoggedIn &&
             (email === "nancy@test.com" || email === "test@test.com") && (
               <li>
-                <Link
-                  className={btnClassesNancy}
-                  onClick={clickHandlerNancy}
-                  to="/Nancy"
-                >
-                  Nancy
-                </Link>
+                {window.innerWidth > 768 ? (
+                  <Link
+                    className={btnClassesNancy}
+                    onClick={clickHandlerNancy}
+                    to="/Nancy"
+                  >
+                    Nancy
+                  </Link>
+                ) : (
+                  <Link
+                    className={classes.btnChico}
+                    onClick={clickHandlerNancy}
+                    to="/Nancy"
+                  >
+                    N
+                  </Link>
+                )}
               </li>
             )}
         </ul>
@@ -151,7 +197,7 @@ const MainNavigation = () => {
           )}
           {isLoggedIn && (
             <li>
-              <Link to="/profile">Profile</Link>
+              {window.innerWidth > 768 && <Link to="/profile">Profile</Link>}
             </li>
           )}
           {isLoggedIn && (
