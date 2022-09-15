7c6f86480c796265eb20f9a42a97420471587450
margarita <mzerpa@tecnosoftware.com.ar>
2022-09-15 14:20:05 -0300

    cambios en presentacion marga

---
+++ src/components/Marga/footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./footer.css";
+export const footer = () => {
+  return (
+    <footer className='footer'>
+        <div className='footer-info'>
+            <h1>Tecnosoftware</h1>
+        </div>
+    </footer>
+  )
+}
+export default footer;
